33c1ee0264fa4d795d7eb28d11e5b8bfb7d06ecd
hanna13n <hanna.nechikkadan@gmail.com>
2021-07-03 07:02:22 +0530

    jquery and apification

---
+++ 22-june/jQuery/jobs/static/main.js
@@ -0,0 +1,14 @@
+function handleUpdate(resp) {
+    console.log(resp);
+    $("#details").html(resp);
+}
+function handleClick(event) {
+    var link = event.target;
+    console.log(link['href']);
+    $.get(link).done(handleUpdate);
+    event.preventDefault();
+}
+function main() {
+    $("a.joblink").click(handleClick);
+}
+$(main);
